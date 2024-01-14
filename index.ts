740d1e50f164bc4b370fcead82714d35851c6661
Kristof Gazso <kristof.gazso@gmail.com>
2024-01-14 14:36:37 +0000

    update versions

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv"
-import { getAccountNonce } from "permissionless"
+import { getAccountNonce, createSmartAccountClient } from "permissionless"
 import { UserOperation, bundlerActions, getSenderAddress, getUserOperationHash, waitForUserOperationReceipt, GetUserOperationReceiptReturnType, signUserOperationHashWithECDSA } from "permissionless"
 import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
 import { Address, Hash, concat, createClient, createPublicClient, encodeFunctionData, http, Hex } from "viem"
 import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
-import { lineaTestnet, polygonMumbai } from "viem/chains"
+import { lineaTestnet, polygonMumbai, sepolia } from "viem/chains"
+import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
+import { privateKeyToSimpleSmartAccount } from "permissionless/accounts";
 
 console.log("Hello world!")
