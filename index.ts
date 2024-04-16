2efb99d170b3879cfb3e7404191fc9992bd08aa6
Kristof Gazso <kristof.gazso@gmail.com>
2024-04-16 16:16:37 -0700

    format imports

--- index.ts
+++ index.ts
@@ -1,12 +1,36 @@
 import "dotenv/config"
-import { getAccountNonce, createSmartAccountClient, ENTRYPOINT_ADDRESS_V07 } from "permissionless"
-import { UserOperation, bundlerActions, getSenderAddress, getUserOperationHash, waitForUserOperationReceipt, GetUserOperationReceiptReturnType, signUserOperationHashWithECDSA } from "permissionless"
+import { writeFileSync } from 'fs'
+import {
+    ENTRYPOINT_ADDRESS_V07, 
+    GetUserOperationReceiptReturnType, 
+    UserOperation,
+    bundlerActions, 
+    createSmartAccountClient, 
+    getAccountNonce, 
+    getSenderAddress, 
+    getUserOperationHash, 
+    signUserOperationHashWithECDSA, 
+    waitForUserOperationReceipt
+} from "permissionless"
+import { 
+    privateKeyToSafeSmartAccount, 
+    privateKeyToSimpleSmartAccount, 
+    signerToSafeSmartAccount 
+} from "permissionless/accounts"
 import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
-import { Address, Hash, concat, createClient, createPublicClient, encodeFunctionData, http, Hex } from "viem"
+import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico"
+import { 
+    Address, 
+    Hash, 
+    Hex, 
+    concat, 
+    createClient, 
+    createPublicClient, 
+    encodeFunctionData, 
+    http, 
+    parseAbiItem 
+} from "viem"
 import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
 import { lineaTestnet, polygonMumbai, sepolia } from "viem/chains"
-import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
-import { privateKeyToSimpleSmartAccount, privateKeyToSafeSmartAccount } from "permissionless/accounts";
-import { writeFileSync } from 'fs'
 
 console.log("Hello world!")
